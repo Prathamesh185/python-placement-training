a5dc005cd3a0cf0f4fa8e1deedb97823e22c16f9
Prathamesh Patil <prathameshkoph@gmail.com>
2026-03-26 09:17:34 +0530

    made corrections to files

--- Day2/joinMethod.py
+++ Day2/joinMethod.py
@@ -2,6 +2,8 @@
 # m = '|'.join(s)
 # print(m)
 
+
+#find() returns the starting index of a string and if string is not present then it returns default value -1
 s = "help4code is a best platform for practicing programming"
 print(s.find("help4code"))
 print(s.find("python"))
